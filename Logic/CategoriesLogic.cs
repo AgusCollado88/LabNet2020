b85b21b55a7ae358f53355be4b09fd4f425f1498
Collado, Agustin <agus.tahiel.collado@gmail.com>
2020-11-01 20:19:41 -0300

    modificacion en Categories

--- Logic/CategoriesLogic.cs
+++ Logic/CategoriesLogic.cs
@@ -7,13 +7,22 @@ using System.Threading.Tasks;
 
 namespace Logic
 {
+
     public class CategoriesLogic
     {
+        private readonly NorthwindContext context;
+
+        public CategoriesLogic()
+        {
+            this.context = new NorthwindContext();
+        }
+
         public List<Categories> Categories()
         {
             NorthwindContext context = new NorthwindContext();
             return context.Categories.ToList();
         }
 
+        
     }
 }
