0aa210f6649c271076446a104ed2ef4c41117dff
Collado, Agustin <agus.tahiel.collado@gmail.com>
2020-11-16 16:39:22 -0300

    se subieron sin querer todas mis pruebas antes de la que use finalmente, hago Delete de eso

--- API-HowIMetYourMother/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace API_HowIMetYourMother.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- API-HowIMetYourMother/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="API_HowIMetYourMother.WebApiApplication" Language="C#" %>
